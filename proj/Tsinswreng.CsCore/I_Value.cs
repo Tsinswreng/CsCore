7fe860954863456ca2c985b4e568f47c167b7e23
Tsinswreng <Tsinswreng@gmail.com>
2026-05-17 14:10:34 +0800

    I_Value改註

--- proj/Tsinswreng.CsCore/I_Value.cs
+++ proj/Tsinswreng.CsCore/I_Value.cs
@@ -1,10 +1,12 @@
+using System.Diagnostics.Contracts;
+
 namespace Tsinswreng.CsCore;
 
 
 /// 不建議單獨叶此接口。璫叶IDictSerializable
 public partial interface I_ToSerialized{
 #if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_0_OR_GREATER || NET5_0_OR_GREATER
-	//pure fn
+	[Pure]
 	public obj? ToSerialized(obj? Obj){
 		return Obj;
 	}
@@ -14,7 +16,7 @@ public partial interface I_ToSerialized{
 /// 不建議單獨叶此接口。璫叶IDictSerializable
 public partial interface I_ToDeSerialized{
 #if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_0_OR_GREATER || NET5_0_OR_GREATER
-	//pure fn
+	[Pure]
 	public obj? ToDeSerialized(obj? Obj){
 		return Obj;
 	}
@@ -29,12 +31,15 @@ public partial interface IDictSerializable
 }
 
 
-/// 自封裝之值類型需叶斯接口、㕥便序列化時免包一層{"Value": xxx}
+[Doc($$"""
+自封裝之值類型需叶斯接口、㕥便序列化時免包一層`{"Value": xxx}`
+""")]
 public partial interface I_ValueObj{
 	public obj? ValueObj{get;set;}
 }
 
 
+[Doc(@$"generic version for {nameof(I_ValueObj)}")]
 public partial interface I_Value<T>:I_ValueObj{
 	public T Value{get;set;}
 #if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_0_OR_GREATER || NET5_0_OR_GREATER
